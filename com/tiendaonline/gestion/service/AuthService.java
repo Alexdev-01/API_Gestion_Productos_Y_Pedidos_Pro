fa812d092b89defc2eba67e838e55f46e6af3887
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-20 15:35:01 +0100

    Add files via upload

--- com/tiendaonline/gestion/service/AuthService.java
+++ com/tiendaonline/gestion/service/AuthService.java
@@ -1,1 +1,15 @@
+package com.tiendaonline.gestion.service.impl;
 
+package com.tiendaonline.gestion.service;
+
+import com.tiendaonline.gestion.dto.auth.AuthResponse;
+import com.tiendaonline.gestion.dto.auth.LoginRequest;
+import com.tiendaonline.gestion.dto.auth.RegisterRequest;
+
+public interface AuthService {
+
+	AuthResponse register(RegisterRequest request);
+
+	AuthResponse login(LoginRequest request);
+	
+}

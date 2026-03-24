68cdffc119be01468ecf725cf73d15c46c8306f2
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-24 11:00:06 +0100

    Add files via upload

--- com/tiendaonline/gestion/controller/AuthController.java
+++ com/tiendaonline/gestion/controller/AuthController.java
@@ -1,1 +1,40 @@
+package com.tiendaonline.gestion.controller;
 
+import com.tiendaonline.gestion.dto.auth.AuthResponse;
+import com.tiendaonline.gestion.dto.auth.LoginRequest;
+import com.tiendaonline.gestion.dto.auth.RegisterRequest;
+import com.tiendaonline.gestion.service.AuthService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController		// Marca esta clase como un controlador REST, lo que permite manejar solicitudes HTTP y devolver respuestas en formato JSON
+@RequestMapping("/auth")	// Define la ruta base para todas las solicitudes manejadas por este controlador, lo que significa que todas las rutas definidas en este controlador comenzarán con "/auth"
+// Controlador para manejar las solicitudes de autenticación, como el registro y el inicio de sesión de los usuarios. Utiliza el servicio de autenticación para procesar las solicitudes y generar respuestas adecuadas.
+public class AuthController {
+	
+	private final AuthService authService;
+	
+	public AuthController(AuthService authService) {
+		this.authService = authService;
+	}
+	
+	
+	@PostMapping("/register")	// Define una ruta para manejar las solicitudes de registro de usuarios, lo que significa que las solicitudes POST a "/auth/register"
+	//	Recibe un objeto RegisterRequest en el cuerpo de la solicitud, lo que permite obtener los datos necesarios para registrar a un nuevo usuario. El método devuelve una respuesta con un objeto AuthResponse que contiene el token JWT generado para el usuario registrado.
+	public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
+		
+		return ResponseEntity.ok(authService.register(request));
+	}
+	
+	@PostMapping("/login")
+	//	Recibe un objeto LoginRequest en el cuerpo de la solicitud, lo que permite obtener los datos necesarios para autenticar a un usuario. El método devuelve una respuesta con un objeto AuthResponse que contiene el token JWT generado para el usuario autenticado.
+	public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
+		
+		return ResponseEntity.ok(authService.login(request));
+	}
+	
+}

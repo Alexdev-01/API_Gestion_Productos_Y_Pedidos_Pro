0b790d243835a90a7807b218ff82d4471e455052
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-03-27 09:44:46 +0100

    Add files via upload

---
+++ com/tiendaonline/gestion/dto/auth/LoginRequest.java
@@ -0,0 +1,32 @@
+package com.tiendaonline.gestion.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+
+// Solicitud de inicio de sesión (login) en la aplicación.
+public class LoginRequest {
+	
+	@NotBlank  // Asegura que el campo no esté vacío o solo contenga espacios en blanco
+	private String username;
+	
+	@NotBlank
+	private String password;
+
+	public LoginRequest() {
+		super();
+	}
+
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+
+}

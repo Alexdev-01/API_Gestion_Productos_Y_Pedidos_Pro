f5ab5db78ec17a6c4735220a2022bf4d675368a8
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-04-09 14:32:51 +0200

    Add files via upload

--- com/tiendaonline/gestion/repository/CategoriaRepository.java
+++ com/tiendaonline/gestion/repository/CategoriaRepository.java
@@ -4,9 +4,8 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.tiendaonline.gestion.model.Categoria;
-import com.tiendaonline.gestion.model.Producto;
 
-public interface CategoriaRepository extends JpaRepository<Producto, Long>{
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
 	
 	Optional<Categoria> findByNombre(String nombre);
 

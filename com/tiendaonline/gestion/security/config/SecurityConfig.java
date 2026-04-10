580a5eb572eaea44bc472990b7ceb0768618c825
Alejandro Collado Severiano <colladoacs@gmail.com>
2026-04-10 14:16:51 +0200

    Add files via upload

--- com/tiendaonline/gestion/security/config/SecurityConfig.java
+++ com/tiendaonline/gestion/security/config/SecurityConfig.java
@@ -6,40 +6,38 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.tiendaonline.gestion.ApiGestionProductosYPedidosApplication;
 import com.tiendaonline.gestion.security.jwt.JwtAuthenticationFilter;
 
 @Configuration	// Marca esta clase como una clase de configuración de Spring, lo que permite definir beans y configuraciones relacionadas con la seguridad
 @EnableWebSecurity	// Habilita la configuración de seguridad web en la aplicación, lo que permite personalizar la seguridad de las solicitudes HTTP
 public class SecurityConfig {
-
-    private final ApiGestionProductosYPedidosApplication apiGestionProductosYPedidosApplication;
 	
 	private final JwtAuthenticationFilter jwtFilter;
 
-	public SecurityConfig(JwtAuthenticationFilter jwtFilter, ApiGestionProductosYPedidosApplication apiGestionProductosYPedidosApplication) {
-		super();
+	public SecurityConfig(JwtAuthenticationFilter jwtFilter) {
 		this.jwtFilter = jwtFilter;
-		this.apiGestionProductosYPedidosApplication = apiGestionProductosYPedidosApplication;
 	}
 	
 	@Bean	// Marca este método como un bean de Spring, lo que permite su gestión y uso en otras partes de la aplicación
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
-		http.csrf().disable().authorizeHttpRequests(auth -> auth
+		http
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/auth/**").permitAll()
 				.requestMatchers("/productos/**").permitAll()
 				.requestMatchers("/categoria/**").hasRole("ADMIN")
 				.anyRequest().authenticated()
-				)
-				.sessionManagement (session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				)
-				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+			)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 		
 		return http.build();
 	}
